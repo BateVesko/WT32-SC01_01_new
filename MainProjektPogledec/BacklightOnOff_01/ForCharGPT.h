d7d085eb8bc792927bcff44f5ccf2bb366b2cea3
BateVesko <133985940+BateVesko@users.noreply.github.com>
2024-07-22 20:53:12 +0300

    Update22072055
    All

--- MainProjektPogledec/BacklightOnOff_01/ForCharGPT.h
+++ MainProjektPogledec/BacklightOnOff_01/ForCharGPT.h
@@ -1,4 +1,4 @@
-1.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/BacklightOnOff/ForCharGPT.h
-2.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/BacklightOnOff/BacklightOnOff.ino
-3.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/BacklightOnOff/soft_backlight.h
-4.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/BacklightOnOff/touch_utils.h
+1.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/BacklightOnOff_01/ForCharGPT.h
+2.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/BacklightOnOff_01/BacklightOnOff.ino
+3.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/BacklightOnOff_01/soft_backlight.h
+4.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/BacklightOnOff_01/touch_utils.h
