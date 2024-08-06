d180e0c8edc40c8403596b9fa4d66417de435c14
BateVesko <133985940+BateVesko@users.noreply.github.com>
2024-08-06 17:13:53 +0300

    Update06081715
    ll

--- MainProjektPogledec/Project_05_0608_11705/010_ForChatGPT.h
+++ MainProjektPogledec/Project_05_0608_11705/010_ForChatGPT.h
@@ -1,15 +1,15 @@
-000.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0308_1845/Project_05_26_2030.ino
-010.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0308_1845/010_ForChatGPT.h
-020.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0308_1845/020_display_utils.h
-030.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0308_1845/030_touch_utils.h
-040.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0308_1845/040_rotate_icon.h
-050.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0308_1845/050_frame_generator.h
-060.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0308_1845/060_variable_set_triangles.h
-070.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0308_1845/070_HTTPServer.h
-080.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0308_1845/080_PWM_generator.h
-090.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0308_1845/090_ColorUtils.h
-100.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0308_1845/100_memory_utils.h
-110.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0308_1845/110_images.h
+000.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0608_11705/Project_05_26_2030.ino
+010.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0608_11705/010_ForChatGPT.h
+020.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0608_11705/020_display_utils.h
+030.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0608_11705/030_touch_utils.h
+040.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0608_11705/040_rotate_icon.h
+050.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0608_11705/050_frame_generator.h
+060.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0608_11705/060_variable_set_triangles.h
+070.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0608_11705/070_HTTPServer.h
+080.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0608_11705/080_PWM_generator.h
+090.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0608_11705/090_ColorUtils.h
+100.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0608_11705/100_memory_utils.h
+110.https://github.com/BateVesko/WT32-SC01_01_new/blob/main/MainProjektPogledec/Project_05_0608_11705/110_images.h
 
 // A1_6531 -> 48575443BD107EA3
 // TP-Link_1AEA -> 83590566
